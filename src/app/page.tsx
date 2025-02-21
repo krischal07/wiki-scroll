895d3ccb9d73927f92cfa0637579ff400ae9b944
krischal07 <krischal.shrestha9849@gmail.com>
2025-02-21 19:10:41 +0545

    connected with api of wiki

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,148 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+type WikiPage = {
+  pageid:number,
+  title:string,
+  extract: string
 }
+const Home = () => {
+  const [keyword, setKeyword] = useState("")
+  const[search, setSearch] = useState("")
+  const[loading, setLoading] = useState(false)
+  const [info, setInfo] = useState("")
+  const[image, setImage] = useState("")
+  const [text, setText] = useState("")
+
+  // const [info, setInfo] = useState<WikiPage | null>(null)
+  console.log(search)
+  console.log(keyword,"keyword")
+
+  // console.log("heelo")
+  // useEffect(()=>{
+  //     const fetchData = async ()=>{
+  //       const response = await fetch(`https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles=${keyword}`)
+  //       const data = await response.json()
+  //       const pages = Object.values(data.query.pages) as WikiPage[]
+  //       setInfo(pages[0])
+  //       // setInfo(data)
+  //       // console.log(info)
+  //     }
+  //     if(search){
+
+  //       fetchData()
+  //     }
+  // },[keyword])
+
+  // const searchWiki = async()=>{
+  //   // setKeyword(search)
+  //   setLoading(true)
+  //   if(!keyword) return
+  //   try {
+  //     const url = "https://en.wikipedia.org/w/api.php"
+  //     const params = new URLSearchParams({
+  //       action:'query',
+  //       list:"search",
+  //       srsearch:keyword,
+  //       // prop:"images",
+  //       format:"json",
+  //       origin: "*"
+  //     })
+      
+  //     const res = await fetch(`${url}?${params}`)
+  //     const data = await res?.json()
+      
+  //     if((data?.query?.search[0]?.title)){
+  //       console.log(data)
+  //       setPages(data?.query?.search )
+  //     }
+  //   } catch (error) {
+  //     console.log(error)
+  //   }finally{
+  //     setLoading(false)
+  //   }
+
+  // }
+  // const showPage = async (page)=>{
+  //   console.log(page)
+  //   setLoading(true)
+  //   try {
+  //     const url = `https://en.wikipedia.org/w/api.php?action=parse&page=${page?.title}&prop=text&formatversion=2&origin=*&format=json`
+  //     const res = await fetch(url)
+  //     const data = await res.json()
+  //     console.log(data,"data")
+  //     setInfo(data)
+  //     // console.log(info,"info")
+  //   } catch (error) {
+  
+  //   }
+  // }
+
+  useEffect(()=>{
+      const fetchWiki =async ()=>{
+          setLoading(true)
+          try {
+                const url = `https://en.wikipedia.org/w/api.php?action=parse&page=${keyword}&prop=text&formatversion=2&origin=*&format=json`
+                const res = await fetch(url)
+                const data = await res.json()
+                console.log(data,"data")
+                setInfo(data?.parse?.text)
+                // Extracting the first paragraphn only
+                const pageHtml = data?.parse?.text
+                const tempDiv = document.createElement("div")
+                tempDiv.innerHTML = pageHtml
+
+                // Remove unwanted elements
+                 tempDiv.querySelectorAll("table, style, script, .hatnote").forEach((el) => el.remove());
+
+                 const firstPara = [...tempDiv.querySelectorAll("p")]
+                 .find((p) => p.innerText.trim().length > 1)?.innerText || "No content found.";
+       
+                console.log(firstPara, "firstPara")
+                console.log(pageHtml, "pageHtml")
+                setText(firstPara)
+
+                // Extracting Images only
+                const firstImg = tempDiv.querySelector("img")
+                let imgUrl = ""
+                if(firstImg){
+                  const src = firstImg.getAttribute("src")
+                  if(src){
+                    imgUrl = src.startsWith("//") ? `https:${src}` : `https://en.wikipedia.org${src}`;
+
+                  }
+                }
+                setImage(imgUrl)
+                
+              } catch (error) {
+                console.log(error)
+              }finally{
+                setLoading(false)
+              }
+      }
+      fetchWiki()
+
+  },[keyword])
+  return (
+  
+    <div>
+      <div className='flex m-5'>
+      <Input type='text' placeholder='Search for anything...' className='w-56 mr-4' onChange={(e)=>setSearch(e.target.value)}/>
+      <Button onClick={()=>setKeyword(search)}>Search</Button>
+
+      </div>
+      {loading===true?<div>Loading.....</div>:<div>
+        {/* <div dangerouslySetInnerHTML={{__html:info}}/> */}
+        {image && <img src={image} style={{width:"300px", borderRadius:"8px"}}/>}
+        <p className='border-2 border-red-500'>{text}</p>
+      </div>}
+      
+   
+    </div>
+  )
+}
+
+export default Home
